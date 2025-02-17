162e74c170bcf4c7f3b42e6dea75ad4bb197cbb1
shaq <shaquxn@gmail.com>
2025-02-17 14:13:39 -0400

    updated files for npm run build to work

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
     images: {  // Keep your existing images configuration
         domains: ['lh3.googleusercontent.com', 'insta-recipe-assets.s3.us-east-1.amazonaws.com'],
     },
