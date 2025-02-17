f8c173dfca661ad04ba0298824e80bb524db3734
shaq <shaquxn@gmail.com>
2025-02-17 13:33:30 -0400

    updated next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Add this line to force static HTML export
     reactStrictMode: true,
     swcMinify: true,
     images: {  // Keep your existing images configuration
